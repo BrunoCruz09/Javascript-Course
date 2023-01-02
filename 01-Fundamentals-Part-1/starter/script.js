b845c74fb4724d8521a393f23fbc45c188a8760f
BrunoCruz09 <brunobarroso@ipcbcampus.pt>
2023-01-02 22:04:08 +0000

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,18 @@
 let js = "amazing";
-if (js === "amazing") alert("JS FUN");
-
-40 + 8 + 23 - 10;
-
 console.log(40 + 8 + 23 - 10);
+
+//Value -> Pass literal value
+console.log("Jonas");
+
+//Variable -> Pass variable
+let firstName = "jonas";
+console.log(firstName);
+
+//Lecture Value and Variables
+let country = "Portugal";
+let continent = "Europa";
+let population = "10.3M";
+
+console.log(country);
+console.log(continent);
+console.log(population);
