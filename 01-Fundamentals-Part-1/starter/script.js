5fec727511898dd2958af9f8a77a4bb0b2db2721
BrunoCruz09 <brunobarroso@ipcbcampus.pt>
2023-01-02 22:20:04 +0000

    Data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/* VALUE AND VARIABLES
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -7,12 +8,51 @@ console.log("Jonas");
 //Variable -> Pass variable
 let firstName = "jonas";
 console.log(firstName);
+*/
 
 //Lecture Value and Variables
 let country = "Portugal";
 let continent = "Europa";
 let population = "10.3M";
-
 console.log(country);
 console.log(continent);
 console.log(population);
+
+/**
+ * DATA TYPES
+ *
+ * 1-Number -> use for decimal and integers
+ * 2-String -> used for text
+ * 3-Boolean -> used for taking decisions
+ * 4-Undifined ->Value taken by a variable that is not yet defined
+ * 5-Null -> Also means 'empty value'
+ * 6-Symbol (ES2015) -> larger integers than the Number type can hold
+ * 7-BigInt (ES202) -> Large integers tha the Number type can hold
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "jonas");
+
+javascriptIsFun = "YES";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+ */
+
+//Lecture Data types
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
