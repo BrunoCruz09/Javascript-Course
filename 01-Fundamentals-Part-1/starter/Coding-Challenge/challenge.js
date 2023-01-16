c45268a2f1150ccbc6e7326d4a607ac910e2a905
BrunoCruz09 <brunobarroso@ipcbcampus.pt>
2023-01-16 23:00:08 +0000

    Coding Challenge #3

--- 01-Fundamentals-Part-1/starter/Coding-Challenge/challenge.js
+++ 01-Fundamentals-Part-1/starter/Coding-Challenge/challenge.js
@@ -1,7 +1,7 @@
 /**
- * Challenge One
+ * CHALLENGE ONE
  */
-const markMass = 68;
+/*const markMass = 68;
 const johnMass = 70;
 const markHeigh = 1.9;
 const johnHeigh = 1.88;
@@ -10,13 +10,51 @@ const BMIMark = markMass / markHeigh ** 2;
 const BMIJohn = johnMass / (johnHeigh * johnHeigh);
 const markHigherMBI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn);
-console.log(markHigherMBI);
+console.log(markHigherMBI);*/
 
 /**
- * Challenge Two
+ * CHALLENGE TWO
  */
-if (BMIMark > BMIJohn) {
+/*if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
 } else {
   console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
-}
+}*/
+
+/**
+ * CHALLENGE THREE
+ */
+/*
+const averageDolphins = (96+108+89)/3;
+const averagekoalas = (88+91+110)/3;
+console.log('Dolphins: ' + averageDolphins +" | "+ 'Koalas: ' + averagekoalas)
+
+if (averageDolphins === averagekoalas){
+  console.log("Both team have the same average score");
+} else if (averageDolphins > averagekoalas) {
+  console.log('Dolphins win!');
+}else {
+  console.log('Koalas win!');
+}*/
+
+/*BONUS 1
+const averageDolphins = (97+112+101)/3;
+const averagekoalas = (109+95+123)/3;
+console.log('Dolphins: ' + averageDolphins +" | "+ 'Koalas: ' + averagekoalas)
+
+if (averageDolphins > averagekoalas && averageDolphins >= 100){
+  console.log("Dolphins win");
+} else if (averageDolphins < averagekoalas && averagekoalas >= 100) {
+  console.log('Koalas win!');
+}else {
+  console.log('Both team have the same average score');
+}*/
+
+/*BONUS 2
+const averageDolphins = (97+112+101)/3;
+const averagekoalas = (109+95+106)/3;
+console.log('Dolphins: ' + averageDolphins +" | "+ 'Koalas: ' + averagekoalas)
+
+if (averageDolphins === averagekoalas && averageDolphins >= 100 && averagekoalas >= 100){
+  console.log("Both team have the same average score");
+}*/
