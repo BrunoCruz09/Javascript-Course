f96d1b37b84aa0c6ffbbfbfb6e373e8b9b64d252
Bruno Cruz <brunocruz@Air-de-Bruno.Home>
2023-01-24 21:41:23 +0000

    Strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+let hasDriversLicense = false;
+const pass = true;
+
+if(pass) hasDriversLicense = true;
+
+if(hasDriversLicense) console.log('I can drive');
+
+let interface = 'Audio';
+const private = 435;
+ */
