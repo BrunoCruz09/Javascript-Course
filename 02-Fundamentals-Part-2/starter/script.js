35958c02c94ec41ce0deed45adeb69816ce1a08b
Bruno Cruz <brunocruz@Air-de-Bruno.Home>
2023-01-24 22:35:14 +0000

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,7 @@
 'use strict';
+function logger(message) {
+    console.log(message);
+}
 /**
 let hasDriversLicense = false;
 const pass = true;
@@ -9,4 +12,33 @@ if(hasDriversLicense) console.log('I can drive');
 
 let interface = 'Audio';
 const private = 435;
+ */
+//-----------------------------------------------------------------------------
+/**
+ * FUNCTIONS
+
+function logger(message) {
+    console.log(message);
+}
+//calling / running / invoking function
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+logger(fruitProcessor(5, 0));
+
+logger(fruitProcessor(2, 4));
+*/
+
+/**
+ * LECTURE: Functions
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} people and its capital city is ${capitalCity}`
+}
+
+logger(describeCountry('Finland', '6 million', 'helsinki'));
+logger(describeCountry('Portugal', '11 million', 'Lisboa'));
+logger(describeCountry('Spain', '46 million', 'Madrid'));
  */
