86c570231550e545ffbbbcee43bae168414ef9e9
Bruno Cruz <brunocruz@Air-de-Bruno.Home>
2023-01-25 21:53:07 +0000

    Functions calling other Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -110,3 +110,33 @@ logger(yearsUntilRetirement(1990, 'Jonas'));
 const percentageOfWorld2 = population => (population / 7900) * 100 + '%'
 logger(percentageOfWorld2(3950));
 */
+//-----------------------------------------------------------------------------
+/**
+ * Functions calling other Functions
+
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges)
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+    return juice;
+}
+logger(fruitProcessor(2,4));
+*/
+
+/**
+ * LECTURE Functions calling other Functions
+
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100 + '%';
+}
+const population = (country, population) => {
+    const percentageWorld = percentageOfWorld1(population)
+    return `${country} has ${population} million people, which is about ${percentageWorld} of the world`
+}
+logger(population('China', 1441));
+*/
