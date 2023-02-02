e6e3395e48691021d5ab4a9db4bd391921e448db
brunocruz <brunomcbarroso@outlook.pt>
2023-02-02 23:59:03 +0000

    Challenge Two

--- 02-Fundamentals-Part-2/starter/Coding-Challenge/challenge.js
+++ 02-Fundamentals-Part-2/starter/Coding-Challenge/challenge.js
@@ -18,3 +18,26 @@ const checkWinner = (avgDolphins, avgkoalas) => {
 checkWinner(averageDolphins, averagekoalas)
 */
 //-----------------------------------------------------------------------------
+/**
+ * Challenge Two
+
+const calcTip = (bill) => {
+    const tip = bill >= 50 && bill <= 300 ? 15/100 : 20/100
+    return bill >= 50 && bill <= 300 ? bill * tip : bill * tip
+}
+logger(calcTip(100))
+
+const bills = [125, 555, 44]
+calcTip(bills[0])
+calcTip(bills[1])
+calcTip(bills[2])
+
+const tips = []
+tips.push(calcTip(bills[0]))
+tips.push(calcTip(bills[1]))
+tips.push(calcTip(bills[2]))
+logger(tips)
+
+const total = [bills[0] + tips[0],bills[1] + tips[1], bills[2] + tips[2]]
+logger(total)
+ */
