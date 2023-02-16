688b752d4795eb8f2aecad10aad16c14265551c0
brunocruz <brunomcbarroso@outlook.pt>
2023-02-16 22:09:55 +0000

    Challenge Four

--- 02-Fundamentals-Part-2/starter/Coding-Challenge/challenge.js
+++ 02-Fundamentals-Part-2/starter/Coding-Challenge/challenge.js
@@ -74,3 +74,36 @@ if (markInformation.bmi > johnInformation.bmi){
     logger(`${johnInformation.fullName} BMI ${johnInformation.bmi} is higher than ${markInformation.fullName} ${markInformation.bmi}`)
 }
  */
+//-----------------------------------------------------------------------------
+/**
+ *
+ * Challenge Four
+
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = []
+const totals = []
+const calcTip = (bill) => {
+    const tip = bill >= 50 && bill <= 300 ? 15/100 : 20/100
+    return bill >= 50 && bill <= 300 ? bill * tip : bill * tip
+}
+
+for (let i = 0; i < bills.length; i++){
+    tips.push(calcTip(bills[i]))
+    totals.push(bills[i] + tips[i])
+}
+logger(bills)
+logger(tips)
+logger(totals)
+logger('-------------')
+//BONUS
+function calcAverage(arr) {
+    let sum = 0
+    for (let  i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    return sum/arr.length
+}
+
+logger(calcAverage(totals))
+*/
