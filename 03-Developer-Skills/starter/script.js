d4e4e40bfca247efe103d383f0022add8cbfcb7c
brunocruz <brunomcbarroso@outlook.pt>
2023-02-17 00:06:44 +0000

    Forecast challenge and min and max value of array

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,33 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const arrayOne = [17, 21, 23]
+const arrayTwo = [12, 5, -5, 0, 4]
+const printForecast = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        console.log(`${arr[i]}ºC in ${[i + 1]} days`)
+    }
+}
+
+printForecast(arrayTwo)
+console.log('------------')
+const temperature = [14,33,6,34,1,27,41];
+const temp = (temp) =>{
+    let maxValue = temp[0];
+    let minValue = temp[0];
+    /*return max value
+    return Math.max.apply(null, temp)*/
+
+    /*return min value
+    return Math.min(...temp)*/
+    for(let i = 0; i < temp.length; i++){
+        const curTemp = temp[i]
+
+        if(curTemp > maxValue) maxValue = curTemp
+        if(curTemp < minValue) minValue = curTemp
+    }
+    //console.log( minValue)
+    return [maxValue, minValue]
+}
+
+console.log(temp(temperature))
